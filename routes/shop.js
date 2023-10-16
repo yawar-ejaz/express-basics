ab3e55e928b6b2b6e17a4a1be98b92b7c8cd92ab
Yawar Ejaz <130927411+yawar-ejaz@users.noreply.github.com>
2023-10-16 13:42:36 +0530

    added contact us page

--- routes/shop.js
+++ routes/shop.js
@@ -7,4 +7,12 @@ router.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'home.html'));
 });
 
+router.get('/contactus', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'contact.html'));
+});
+
+router.post('/success', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'success.html'));
+});
+
 module.exports = router;
